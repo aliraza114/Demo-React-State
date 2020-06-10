e363107f64f25cfc1e1adff3db9ec60388697ebf
aliraza114 <aliraza3569@gmail.com>
2020-06-10 21:55:06 +0500

    added css and assignment done

--- src/Room.js
+++ src/Room.js
@@ -1,19 +1,33 @@
 import React, {useState} from 'react';
+import room from './Room.css'
 
 function Room() {
     let [isLit, setLit] = useState(true)
     let [count, setCount] = useState(0)
+    let [temp, setTemp] = useState(73)
     function updateRoomState() {
         setLit(!isLit)
         setCount(++count)
     }
+    function changeLight(value){
+        setLit(isLit = value)
+    }
+    function changeTemp(value) {
+        setTemp( temp += value )
+    }
   return (
-    <div>
+    <div className={`room ${isLit ? 'lit' : 'dark'}`}>
       This is room is { isLit ? 'lit' : 'dark' }
       <br/>
-      <button onClick={updateRoomState}>{ isLit ? 'Off The Light' : 'On the light' }</button>
-      <br/>
-      <button> { 'You switched the light ' + count + ' Times' }</button>
+      <button className={'marginClass'} onClick={updateRoomState}>{ isLit ? 'Off The Light' : 'On the light' }</button>
+      
+      { isLit ? <button className={'marginClass'}> { isLit ? 'The Temperature is ' + temp : '' }</button> : ''}
+    
+      <button className={'marginClass'} onClick={() => changeLight(true)}> ON </button>
+      <button className={'marginClass'} onClick={() => changeLight(false)}> OFF </button>
+      
+      <button className={'marginClass'} onClick={() => changeTemp(1)}> Add Temperature  </button>
+      <button className={'marginClass'} onClick={() => changeTemp(-1)}> Subtract Temperature </button>
     </div>
   );
 }
